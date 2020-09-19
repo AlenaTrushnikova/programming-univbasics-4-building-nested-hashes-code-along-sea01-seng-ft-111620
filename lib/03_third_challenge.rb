b68c795ed4f3189f34a9cdcc3e90a62a4831ca35
Alena Trushnikova <alena.yu.t@gmail.com>
2020-09-19 00:44:40 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -14,13 +14,13 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-        name: "Lord Capulet", age: "53"
+        name: "Lord Capulet", age: "50"
         },
       :matriarch => {
         name: "Lady Capulet", age: "51"
         },
       :heroine => {
-        name: "Romeo", age: "15", status: "alive"
+        name: "Juliet", age: "15", status: "alive"
         },
       :heroine_friends => []
    }
